66e37af1f3243abbe035a34f7f80a0a679e5894c
liamh <liamh@magmadigital.co.uk>
2020-03-12 19:01:59 +0000

    Add pop over when add a new todo item

--- assets/js/vue/components/todoComponent/ItemInput.vue
+++ assets/js/vue/components/todoComponent/ItemInput.vue
@@ -1,23 +1,47 @@
 <template>
-    <input class="form-control" v-model="name" :disabled="saving" v-on:keyup.enter="submit(name)">
+    <div>
+        <input class="form-control" v-model="name" :disabled="saving" v-on:keyup.enter="submit(name)">
+        <b-alert
+                v-bind:variant="alertVariant"
+                fade
+                class="position-fixed fixed-top m-0 rounded-0 text-center"
+                :show="alertCountDown"
+                @dismissed="showAlert=0"
+                @dismiss-count-down="countDownChanged"
+        >
+            {{this.alertMessage}}
+        </b-alert>
+    </div>
 </template>
 
 <script>
     import {itemMixin} from '../../mixins/item';
+    const ALERT_SECONDS = 3;
 
     export default {
         mixins: [itemMixin],
-        data: () => {return {name: null, saving: false}},
+        data: () => {return {name: null, saving: false, alertMessage: '', alertVariant : '', alertCountDown: 0}},
         methods: {
             submit(name) {
                 this.saving = true;
                 this.createItem(name)
                     .then(item => {
                         this.$emit('newItem', item);
+                        this.alertMessage = 'Item Successfully Added!';
+                        this.alertVariant = 'success';
+                        this.alertCountDown = ALERT_SECONDS;
                         this.name = null;
                     })
+                    .catch(response => {
+                        this.alertMessage = 'Something went wrong!';
+                        this.alertVariant = 'danger';
+                        this.alertCountDown = ALERT_SECONDS;
+                    })
                     .finally(()  => this.saving = false);
-            }
+            },
+            countDownChanged(dismissCountDown) {
+                this.alertCountDown = dismissCountDown;
+            },
         }
     };
 </script>
